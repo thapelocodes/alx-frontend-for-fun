d930b1dbb00498dc885f7c73f470f666eb6ca7d9
tm-moumakoe <123243154+tm-moumakoe@users.noreply.github.com>
2024-05-09 11:50:54 +0200

    task 1 fix

--- markdown2html.py
+++ markdown2html.py
@@ -3,7 +3,8 @@
 import os
 import sys
 
-if len(sys.argv) < 2:
+
+if len(sys.argv) < 3:
     sys.exit('Usage: ./markdown2html.py README.md README.html')
 if not os.path.exists(sys.argv[1]):
     sys.exit(f'Missing {sys.argv[1]}')
