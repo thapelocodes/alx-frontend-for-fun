07b358c154ea3787c9ef5ebe33006d1e6bbe0ecd
tm-moumakoe <123243154+tm-moumakoe@users.noreply.github.com>
2024-05-09 12:35:28 +0200

    Headings

--- markdown2html.py
+++ markdown2html.py
@@ -9,3 +9,18 @@ if __name__ == '__main__':
         sys.exit('Usage: ./markdown2html.py README.md README.html')
     if not os.path.isfile(sys.argv[1]):
         sys.exit(f'Missing {sys.argv[1]}')
+
+    with open(sys.argv[1]) as md:
+        with open(sys.argv[2], 'w') as html:
+            for line in md:
+                length = len(line)
+                h = line.lstrip('#')
+                h_lvl = length - len(h)
+
+                if 1 <= h_lvl <= 6:
+                    line = f'<h{h_lvl}>{h.strip()}<h{h_lvl}>\n'
+
+                if length > 1:
+                    html.write(line)
+
+    exit(0)
