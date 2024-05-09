a5d7fce631cda64be4d705d23d9f1e78a7772854
tm-moumakoe <123243154+tm-moumakoe@users.noreply.github.com>
2024-05-09 18:40:12 +0200

    unordered listing

--- markdown2html.py
+++ markdown2html.py
@@ -12,15 +12,28 @@ if __name__ == '__main__':
 
     with open(sys.argv[1]) as md:
         with open(sys.argv[2], 'w') as html:
+            ul_start = False
             for line in md:
                 length = len(line)
                 h = line.lstrip('#')
                 h_lvl = length - len(h)
+                ul = line.lstrip('-')
+                ul_num = length - len(ul)
 
                 if 1 <= h_lvl <= 6:
                     line = f'<h{h_lvl}>{h.strip()}</h{h_lvl}>\n'
 
+                if ul_num:
+                    if not ul_start:
+                        html.write('<ul>\n')
+                        ul_start = True
+                    line = f'<li>{ul.strip()}</li>\n'
+                if ul_start and not ul_num:
+                    html.write('</ul>\n')
+                    ul_start = False
+
                 if length > 1:
                     html.write(line)
-
+            if ul_start:
+                html.write('</ul>\n')
     exit(0)
